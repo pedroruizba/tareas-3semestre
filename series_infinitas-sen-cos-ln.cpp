22f9d3e511e2c5c72d613f73aaa93362bdc5a827
MarioNovelaP <31105898+MarioNovelaP@users.noreply.github.com>
2017-09-20 20:51:24 -0500

    Update series_infinitas-sen-cos-ln.cpp

--- series_infinitas-sen-cos-ln.cpp
+++ series_infinitas-sen-cos-ln.cpp
@@ -17,51 +17,44 @@ int main()
 		
 		switch(opc)
 		{
-			case 1:
-				cout<<"coloca un valor: ";
-                cin>>val;
-				cout<<cos(val);
-				
+		   case 1:
+		          cout<<"coloca un valor: ";
+               		  cin>>val;
+			  cout<<cos(val);	
 			break;
-
 		}
-	
-	}while(opc !=0);
+	}
+	while(opc !=0);
 	return 0;
 }	
-
 void menu()
 {
 
-cout<<"1---> Coseno \n";
+                cout<<"1---> Coseno \n";
 		cout<<"2---> Serie Infinita \n";
 		cout<<"3---> Ceno \n";
 		cout<<"0---> Salir \n";
-cout<<"Coloca un el numero de la opcion: ";
-		
+                cout<<"Coloca un el numero de la opcion: ";		
 }
-
 double cos(double a)
 {
 	double res=1;
 	int sign=-1;
 	double g=2;
-
 	for(int lim=1;lim<=a;lim++)
-{
-	res+=(pow(a,g)/fac(g))*sign;
-	g+=2;
-	sign*=-1;
+        {
+	   res+=(pow(a,g)/fac(g))*sign;
+	   g+=2;
+	   sign*=-1;
 	}
 	return res;
 }
-
 double fac(double d)
 {
 	double fact=1;
 	for(int n=1;n<=d;n++)
 	{
-		fact*=n;
-}
+	   fact*=n;
+	}
 	return fact;
 }
